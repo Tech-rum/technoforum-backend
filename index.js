af775ce49b30ae1c5c098dc16c944349ba8606c0
Ayaan Khan <xd.ayaan@gmail.com>
2023-08-03 15:59:53 +0530

    Student data functionality added

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
-const express = require('express')
+const express = require('express');
+const admin = require('firebase-admin');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const port = process.env.PORT || 5000;
-
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json("Hello You are on Technoforum server");
-})
- 
-app.listen(5000, (port) => {
-    console.log("Server is listening on port: ", 5000);
-})
+// Firebase connection to database ----------------------------------------------//
+
+const serviceAccount = require('./key.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+// Middleware -----------------------------------------------------------------//
+
+app.use(bodyParser.json()); // Add body-parser middleware
+app.use(cors()); // Add cors middleware
+
+// Api Calls -------------------------------------------------------------------//
+
+app.post("/api/register", (req, res) => {
+    const body = req.body; // Parsed request body from body-parser
+
+    var details = {
+        studentname: body.name,
+        email: body.email,
+        phone: body.phoneNo,
+        dob: body.dob,
+        batch: body.batch,
+        year: body.year,
+        semester: body.semester,
+        address: body.address
+    };
+
+    // Save the details to the Firestore database
+    db.collection("userdata").doc(details.year).collection(details.batch).doc(details.semester).set(details)
+        .then(() => {
+            res.status(200).json({ message: "Registration successful!" });
+        })
+        .catch((error) => {
+            res.status(500).json({ error: "Failed to register: " + error.message });
+        });
+});
+
+// Api end --------------------------------------------------------------------//
+
+// Admin panel ----------------------------------------------------------------//
+
+app.listen(port, () => {
+    console.log("Server is listening on port:", port);
+});
